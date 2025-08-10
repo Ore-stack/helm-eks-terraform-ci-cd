4e2ac64aa8ec60687384265b780eb430d553fbd0
Ore-stack <oreoluwaosinowo@yahoo.com>
2025-08-10 10:30:41 +0100

    updated

--- iam.tf
+++ iam.tf
@@ -1,8 +1,10 @@
+# Step 1: Create an IAM user named "github-actions"
 resource "aws_iam_user" "github" {
   name = "github-actions"
 }
 
+# Step 2: Attach the managed policy "AmazonEC2ContainerRegistryPowerUser" to the "github-actions" user
 resource "aws_iam_user_policy_attachment" "github_ecr_access" {
-  user       = aws_iam_user.github.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
+  user       = aws_iam_user.github.name             # Reference the IAM user created above
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"  # AWS managed policy for ECR power user access
 }
